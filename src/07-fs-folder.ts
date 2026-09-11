ff955cca45f314b5538f5b7b4764a4d8f06412b5
Henrique Peixoto Ribeiro <henrique.peixoto.ribeiro50@gmail.com>
2026-09-11 19:47:00 -0300

    More on the fs module

--- src/07-fs-folder.ts
+++ src/07-fs-folder.ts
@@ -6,9 +6,10 @@
 // check file information
 // delete files
 
-// sync apis: fs.readfilesync
-// callback apis
-// promise apis
+// Important modules
+// - sync apis: fs.readfilesync
+// - callback apis
+// - promise apis
 
 // Use cases
 // - Small startup scripts
@@ -22,9 +23,12 @@
 
 import path from "node:path";
 import fs from "node:fs";
+import fsPromise from "node:fs/promises";
 
 const DEMO_FOLDER_PATH = path.join(process.cwd(), 'file-system', 'fs-demo');
 const SYNC_FILE_PATH = path.join(DEMO_FOLDER_PATH, 'sync-note.txt');
+const CALLBACK_FILE_PATH = path.join(DEMO_FOLDER_PATH, 'callback-note.txt');
+const PROMISE_FILE_PATH = path.join(DEMO_FOLDER_PATH, 'promise-note.txt');
 
 type FileResult = {
   style: string;
@@ -54,11 +58,88 @@ function ensureDemoFolderExists(): void {
   }
 }
 
+// Check this callback hell out
+function runCallbackExample(): Promise<FileResult> {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(
+      CALLBACK_FILE_PATH,
+      "created using callback fs",
+      "utf-8",
+      (writeError) => {
+        if (writeError) {
+          reject(writeError);
+          return;
+        }
+
+        fs.appendFile(
+          CALLBACK_FILE_PATH,
+          " Append using callback fs",
+          "utf-8",
+          (appendError) => {
+            if (appendError) {
+              reject(appendError);
+              return;
+            }
+
+            fs.readFile(CALLBACK_FILE_PATH, "utf-8", (readError, content) => {
+              if (readError) {
+                reject(readError);
+                return;
+              }
+
+              fs.stat(CALLBACK_FILE_PATH, (statError, stats) => {
+                if (statError) {
+                  reject(statError);
+                  return;
+                }
+
+                resolve({
+                  style: "callback",
+                  content,
+                  sizeInBytes: stats.size,
+                  fileName: path.basename(CALLBACK_FILE_PATH)
+                })
+              })
+            })
+          }
+        )
+      }
+    )
+  })
+}
+
+// Let's fix the callback hell above
+async function runPromiseExample(): Promise<FileResult> {
+  await fsPromise.writeFile(
+    PROMISE_FILE_PATH,
+    "Created using promise APIs",
+    "utf-8"
+  );
+
+  await fsPromise.appendFile(
+    PROMISE_FILE_PATH,
+    " appended using promise API",
+    "utf-8"
+  );
+
+  const content = await fsPromise.readFile(PROMISE_FILE_PATH, "utf-8");
+  const stats = await fsPromise.stat(PROMISE_FILE_PATH);
+
+  return {
+    style: "promises",
+    content,
+    fileName: path.basename(PROMISE_FILE_PATH),
+    sizeInBytes: stats.size
+  }
+}
+
 async function main(): Promise<void> {
   try {
     ensureDemoFolderExists();
     const syncResult = runSyncExample();
-    console.log([syncResult]);
+    const callbackResult = await runCallbackExample();
+    const promiseResult = await runPromiseExample();
+    console.log([syncResult, callbackResult, promiseResult]);
   } catch (error) {
     const message = error instanceof Error ? error.message : "Unknown error";
     console.error(`File system error! ${message}`);
