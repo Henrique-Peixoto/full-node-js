10398f0084f2695b02fb0c204ad5ea8c43dd261d
Henrique Peixoto Ribeiro <henrique.peixoto.ribeiro50@gmail.com>
2026-09-16 14:48:54 -0300

    Creating a table

---
+++ src/sql/part1/03-first-table.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS basics.students;
+
+CREATE TABLE basics.students (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  age INTEGER CHECK (age >= 18),
+  created_at TIMESTAMP DEFAULT NOW() -- if a value is not provided, is NOW()
+)
