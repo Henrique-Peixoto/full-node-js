ff57ba41d8433d048ed64d535890b3f9570291f3
Henrique Peixoto Ribeiro <henrique.peixoto.ribeiro50@gmail.com>
2026-09-19 10:02:32 -0300

    NULL and IS NOT NULL

---
+++ src/sql/part2/09-null-and-not-null.sql
@@ -0,0 +1,14 @@
+-- null: missing value
+-- Don't do "= null", but rather IS NULL or IS NOT NULL
+
+SELECT name, description
+FROM products
+WHERE description IS NULL;
+
+SELECT name, description
+FROM products
+WHERE description IS NOT NULL;
+
+SELECT name, category, is_active, description
+FROM products
+WHERE is_active = TRUE AND description IS NULL;
