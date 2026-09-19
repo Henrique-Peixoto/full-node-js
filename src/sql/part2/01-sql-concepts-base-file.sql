fdda8ed8faece1a96d7b4c3940281e8b361c00e0
Henrique Peixoto Ribeiro <henrique.peixoto.ribeiro50@gmail.com>
2026-09-19 09:10:40 -0300

    Inserting one line at a time

--- src/sql/part2/01-sql-concepts-base-file.sql
+++ src/sql/part2/01-sql-concepts-base-file.sql
@@ -6,9 +6,9 @@ CREATE TABLE products (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name TEXT NOT NULL,
   category TEXT NOT NULL,
-  price NUMERIC(10,2) NOT NULL CHECK (price >= 0),
-  stock INTEGER NOT NULL CHECK (stock >= 0),
-  is_active BOOLEAN NOT NULL,
+  price NUMERIC(10,2) NOT NULL DEFAULT 0 CHECK (price >= 0),
+  stock INTEGER NOT NULL DEFAULT 0 CHECK (stock >= 0),
+  is_active BOOLEAN NOT NULL DEFAULT true,
   sku TEXT UNIQUE,
   description TEXT,
   created_at TIMESTAMP DEFAULT NOW()
